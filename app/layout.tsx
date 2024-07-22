f352dcfa0eca3422f2a4a0730ef5a24ec613bf12
Lucas Madrigal <lucasmadrigalb@gmail.com>
2024-07-22 02:58:39 -0300

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,6 @@
+import { montserrat } from "./ui/fonts";
+import "./ui/global.css";
+
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +8,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${montserrat.className} antialiased`}>
+        <h1>Holis soy un titulo</h1>
+        {children}
+        <footer className="py-8 flex justify-center align-center bg-gray-100">
+          Hecho con ♥ por el equipo de Next.js 
+        </footer>
+      </body>
     </html>
   );
 }
