2a5a537fa6fc838961a02786e1f67ca91fcecb45
Lucas Madrigal <lucasmadrigalb@gmail.com>
2024-07-28 20:20:45 -0300

    modificando sidenav

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,5 +1,14 @@
 import {Montserrat} from 'next/font/google';
 
+import { Inter, Lusitana } from 'next/font/google';
+ 
+export const inter = Inter({ subsets: ['latin'] });
+ 
+export const lusitana = Lusitana({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+});
+
 export const montserrat = Montserrat({
     subsets: ['latin'],
     variable: '--font-montserrat',
